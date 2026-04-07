82ceefe47bb34191ead3ebda99e01c8e804deb76
jatakuza <jatakuza474@gmail.com>
2026-04-07 19:05:31 +0300

    initial commit

--- bin/untitled1.dart
+++ bin/untitled1.dart
@@ -1,307 +1,57 @@
-import 'dart:math';
-
-import 'package:untitled1/untitled1.dart' as untitled1;
 import 'dart:io';
+import 'dart:math';
+import 'package:characters/characters.dart';
 
-List<String> students = [
-  "Ivan",
-  "Maria",
-  "Alexey",
-  "Olga",
-  "Dmitry",
-  "Anna"
-];
+enum Mood {
+  happy("\u{1F60A}", "Happy", 8),
+  sad("\u{1F622}", "Sad", 3),
+  sleepy("\u{1F62A}", "Sleepy", 2),
+  excited("\u{1F929}", "Excited", 9),
+  angry("\u{1F620}", "Angry", 6);
 
-List<String> subjects = [
-  "Mathematics",
-  "Physics",
-  "Computer Science",
-  "English"
-];
+  final String emoji;
+  final String description;
+  final int energy;
 
-Map<String, Map<String, int>> grades = {
-  "Ivan": {
-    "Mathematics": 5,
-    "Physics": 4,
-    "Computer Science": 5,
-    "English": 3
-  },
-  "Maria": {
-    "Mathematics": 4,
-    "Physics": 5,
-    "Computer Science": 5,
-    "English": 4
-  },
-  "Alexey": {
-    "Mathematics": 3,
-    "Physics": 4,
-    "Computer Science": 4,
-    "English": 3
-  },
-  "Olga": {
-    "Mathematics": 5,
-    "Physics": 5,
-    "Computer Science": 4,
-    "English": 5
-  },
-  "Dmitry": {
-    "Mathematics": 2,
-    "Physics": 3,
-    "Computer Science": 4,
-    "English": 3
-  },
-  "Anna": {
-    "Mathematics": 4,
-    "Physics": 4,
-    "Computer Science": 5,
-    "English": 4
-  }
-};
+  const Mood(this.emoji, this.description, this.energy);
+}
 
-void main(List<String> arguments) {
-  while(true){
-    switch (get_choice()) {
-      case 1:
-        student_list();
+void main() {
+  stdout.write("Enter your name: ");
+  String name = stdin.readLineSync()!;
 
-      case 2:
-        subject_list();
-        break;
+  var random = Random();
+  Mood mood = Mood.values[random.nextInt(Mood.values.length)];
 
-      case 3:
-        students_grades();
-        break;
+  print("Hello, $name! Your mood is: ${mood.emoji} ${mood.description} (energy: ${mood.energy}/10)");
 
-      case 4:
-        average_by_subject();
-        break;
+  int codePoint = mood.emoji.runes.first;
+  print("Emoji Unicode: U+${codePoint.toRadixString(16).toUpperCase()}");
 
-      case 5:
-        average_by_student();
-        break;
+  stdout.write("Do you want to analyze complex emojis? (y/n): ");
+  String answer = stdin.readLineSync()!;
 
-      case 6:
-        best_student();
-        break;
+  if (answer == "y") {
+    stdout.write("Enter a string with emojis: ");
+    String input = stdin.readLineSync()!;
 
-      case 7:
-        worst_subject();
-        break;
+    int codeUnits = input.length;
+    int codePoints = input.runes.length;
+    int realCharacters = input.characters.length;
 
-      case 8:
-        group_average();
-        break;
+    print("\nString analysis:");
+    print("16-bit code units (String.length): $codeUnits");
+    print("Code points (runes): $codePoints");
+    print("Real characters (characters): $realCharacters");
 
-      case 9:
-        subjects_info();
-        break;
+    print("\nUnicode of each character:");
 
-      case 10:
-        students_without_two();
-        break;
+    for (var char in input.characters) {
+      List<String> codes = char.runes
+          .map((r) => "U+${r.toRadixString(16).toUpperCase()}")
+          .toList();
 
-      case 11:
-        students_all_above_four();
-        break;
-
-      case 12:
-        return;
-
-      default:
-        print("Incorrect choice. Try again");
+      print("$char -> ${codes.join(" ")}");
     }
   }
 }
-
-  int get_choice() {
-    print("\nChoose option: ");
-    print("1 - student list");
-    print("2 - subject list");
-    print("3 - students grades");
-    print("4 - average grade by subject");
-    print("5 - average grade by student");
-    print("6 - best student");
-    print("7 - subject with lowest average");
-    print("8 - group average grade");
-    print("9 - subjects list and count");
-    print("10 - students without grade 2");
-    print("11 - students with all grades >= 4");
-    print("12 - Exit");
-    print("Your choice: ");
-
-    int choice = int.parse(stdin.readLineSync()!);
-    return choice;
-  }
-
-  void student_list() {
-    for(int i = 0; i < students.length; i++) {
-      print("${i + 1}: ${students[i]}");
-    }
-  }
-
-  void subject_list() {
-    for(int i = 0; i < subjects.length; i++) {
-      print("${i + 1}: ${subjects[i]}");
-    }
-  }
-
-  void students_grades() {
-    for (var entry in grades.entries) {
-      print("${entry.key}:");
-
-      for (var grade in entry.value.entries) {
-        print("  ${grade.key}: ${grade.value}");
-      }
-    }
-  }
-
-  void average_by_subject() {
-    for (int i = 0; i < subjects.length; i++) {
-      String subject = subjects[i];
-      int sum = 0;
-
-      for (int j = 0; j < students.length; j++) {
-        String student = students[j];
-        sum += grades[student]![subject]!;
-      }
-
-      double avg = sum / students.length;
-      print("$subject average: $avg");
-    }
-  }
-
-  void average_by_student() {
-    for (int i = 0; i < students.length; i++) {
-      String student = students[i];
-      int sum = 0;
-
-      for (int j = 0; j < subjects.length; j++) {
-        String subject = subjects[j];
-        sum += grades[student]![subject]!;
-      }
-
-      double avg = sum / subjects.length;
-      print("$student average: $avg");
-    }
-  }
-
-  void best_student() {
-    String bestStudent = "";
-    double bestAvg = 0;
-
-    for (int i = 0; i < students.length; i++) {
-      String student = students[i];
-      int sum = 0;
-
-      for (int j = 0; j < subjects.length; j++) {
-        String subject = subjects[j];
-        sum += grades[student]![subject]!;
-      }
-
-      double avg = sum / subjects.length;
-
-      if (avg > bestAvg) {
-        bestAvg = avg;
-        bestStudent = student;
-      }
-
-    }
-
-    print("Best student: $bestStudent");
-  }
-
-  void worst_subject() {
-    String worst = "";
-    double worstAvg = 10;
-
-    for (int i = 0; i < subjects.length; i++) {
-      String subject = subjects[i];
-      int sum = 0;
-
-      for (int j = 0; j < students.length; j++) {
-        String student = students[j];
-        sum += grades[student]![subject]!;
-      }
-
-      double avg = sum / students.length;
-
-      if (avg < worstAvg) {
-        worstAvg = avg;
-        worst = subject;
-      }
-    }
-
-    print("Worst subject: $worst");
-  }
-
-  void group_average() {
-    int sum = 0;
-    int counter = 0;
-
-    for (int i = 0; i < students.length; i++) {
-      String student = students[i];
-
-      for (int j = 0; j < subjects.length; j++) {
-        String subject = subjects[j];
-        sum += grades[student]![subject]!;
-        counter++;
-      }
-    }
-
-    double avg = sum / counter;
-    print("Group average: $avg");
-  }
-
-  void subjects_info() {
-    for (int i = 0; i < subjects.length; i++) {
-      print(subjects[i]);
-    }
-
-    print("Total subjects: ${subjects.length}");
-  }
-
-  void students_without_two() {
-    for (int i = 0; i < students.length; i++) {
-      String student = students[i];
-      bool hasTwo = false;
-
-      for (int j = 0; j < subjects.length; j++) {
-        String subject = subjects[j];
-
-        if (grades[student]![subject] == 2) {
-          hasTwo = true;
-        }
-      }
-
-      if (!hasTwo) {
-        print(student);
-      }
-
-    }
-  }
-
-  void students_all_above_four() {
-    for (int i = 0; i < students.length; i++) {
-      String student = students[i];
-      bool good = true;
-
-      for (int j = 0; j < subjects.length; j++) {
-        String subject = subjects[j];
-
-        if (grades[student]![subject]! < 4) {
-          good = false;
-        }
-      }
-
-      if (good) {
-        print(student);
-      }
-
-    }
-
-}
-
-
-
-
-
